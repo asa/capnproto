af521f85799f89bf1b0ee280fb1ecb1366b8833e
Asa Hammond <asa@automaton.is>
2023-09-07 15:11:26 -0700

    disable coroutines in code we don't even use

--- c++/src/kj/async-prelude.h
+++ c++/src/kj/async-prelude.h
@@ -25,13 +25,13 @@
 #pragma once
 
 #include <kj/exception.h>
-#include <kj/tuple.h>
 #include <kj/source-location.h>
+#include <kj/tuple.h>
 
 // Detect whether or not we should enable kj::Promise<T> coroutine integration.
 //
 // TODO(someday): Support coroutines with -fno-exceptions.
-#if !KJ_NO_EXCEPTIONS
+#if !KJ_NO_EXCEPTIONS and false
 #ifdef __has_include
 #if (__cpp_impl_coroutine >= 201902L) && __has_include(<coroutine>)
 // C++20 Coroutines detected.
@@ -86,48 +86,55 @@ using ReducePromises = decltype(reducePromiseType((T*)nullptr, false));
 // reduces Promise<T> to something else. In particular this allows Promise<capnp::RemotePromise<U>>
 // to reduce to capnp::RemotePromise<U>.
 
-template <typename T> struct UnwrapPromise_;
-template <typename T> struct UnwrapPromise_<Promise<T>> { typedef T Type; };
+template <typename T>
+struct UnwrapPromise_;
+template <typename T>
+struct UnwrapPromise_<Promise<T>> {
+    typedef T Type;
+};
 
 template <typename T>
 using UnwrapPromise = typename UnwrapPromise_<T>::Type;
 
 class PropagateException {
-  // A functor which accepts a kj::Exception as a parameter and returns a broken promise of
-  // arbitrary type which simply propagates the exception.
-public:
-  class Bottom {
-  public:
-    Bottom(Exception&& exception): exception(kj::mv(exception)) {}
+    // A functor which accepts a kj::Exception as a parameter and returns a broken promise of
+    // arbitrary type which simply propagates the exception.
+   public:
+    class Bottom {
+       public:
+        Bottom(Exception&& exception) : exception(kj::mv(exception)) {}
 
-    Exception asException() { return kj::mv(exception); }
+        Exception asException() { return kj::mv(exception); }
 
-  private:
-    Exception exception;
-  };
+       private:
+        Exception exception;
+    };
 
-  Bottom operator()(Exception&& e) {
-    return Bottom(kj::mv(e));
-  }
-  Bottom operator()(const  Exception& e) {
-    return Bottom(kj::cp(e));
-  }
+    Bottom operator()(Exception&& e) { return Bottom(kj::mv(e)); }
+    Bottom operator()(const Exception& e) { return Bottom(kj::cp(e)); }
 };
 
 template <typename Func, typename T>
-struct ReturnType_ { typedef decltype(instance<Func>()(instance<T>())) Type; };
+struct ReturnType_ {
+    typedef decltype(instance<Func>()(instance<T>())) Type;
+};
 template <typename Func>
-struct ReturnType_<Func, void> { typedef decltype(instance<Func>()()) Type; };
+struct ReturnType_<Func, void> {
+    typedef decltype(instance<Func>()()) Type;
+};
 
 template <typename Func, typename T>
 using ReturnType = typename ReturnType_<Func, T>::Type;
 // The return type of functor Func given a parameter of type T, with the special exception that if
 // T is void, this is the return type of Func called with no arguments.
 
-template <typename T> struct SplitTuplePromise_ { typedef Promise<T> Type; };
+template <typename T>
+struct SplitTuplePromise_ {
+    typedef Promise<T> Type;
+};
 template <typename... T>
 struct SplitTuplePromise_<kj::_::Tuple<T...>> {
-  typedef kj::Tuple<ReducePromises<T>...> Type;
+    typedef kj::Tuple<ReducePromises<T>...> Type;
 };
 
 template <typename T>
@@ -138,68 +145,82 @@ using SplitTuplePromise = typename SplitTuplePromise_<T>::Type;
 struct Void {};
 // Application code should NOT refer to this!  See `kj::READY_NOW` instead.
 
-template <typename T> struct FixVoid_ { typedef T Type; };
-template <> struct FixVoid_<void> { typedef Void Type; };
-template <typename T> using FixVoid = typename FixVoid_<T>::Type;
+template <typename T>
+struct FixVoid_ {
+    typedef T Type;
+};
+template <>
+struct FixVoid_<void> {
+    typedef Void Type;
+};
+template <typename T>
+using FixVoid = typename FixVoid_<T>::Type;
 // FixVoid<T> is just T unless T is void in which case it is _::Void (an empty struct).
 
-template <typename T> struct UnfixVoid_ { typedef T Type; };
-template <> struct UnfixVoid_<Void> { typedef void Type; };
-template <typename T> using UnfixVoid = typename UnfixVoid_<T>::Type;
+template <typename T>
+struct UnfixVoid_ {
+    typedef T Type;
+};
+template <>
+struct UnfixVoid_<Void> {
+    typedef void Type;
+};
+template <typename T>
+using UnfixVoid = typename UnfixVoid_<T>::Type;
 // UnfixVoid is the opposite of FixVoid.
 
 template <typename In, typename Out>
 struct MaybeVoidCaller {
-  // Calls the function converting a Void input to an empty parameter list and a void return
-  // value to a Void output.
+    // Calls the function converting a Void input to an empty parameter list and a void return
+    // value to a Void output.
 
-  template <typename Func>
-  static inline Out apply(Func& func, In&& in) {
-    return func(kj::mv(in));
-  }
+    template <typename Func>
+    static inline Out apply(Func& func, In&& in) {
+        return func(kj::mv(in));
+    }
 };
 template <typename In, typename Out>
 struct MaybeVoidCaller<In&, Out> {
-  template <typename Func>
-  static inline Out apply(Func& func, In& in) {
-    return func(in);
-  }
+    template <typename Func>
+    static inline Out apply(Func& func, In& in) {
+        return func(in);
+    }
 };
 template <typename Out>
 struct MaybeVoidCaller<Void, Out> {
-  template <typename Func>
-  static inline Out apply(Func& func, Void&& in) {
-    return func();
-  }
+    template <typename Func>
+    static inline Out apply(Func& func, Void&& in) {
+        return func();
+    }
 };
 template <typename In>
 struct MaybeVoidCaller<In, Void> {
-  template <typename Func>
-  static inline Void apply(Func& func, In&& in) {
-    func(kj::mv(in));
-    return Void();
-  }
+    template <typename Func>
+    static inline Void apply(Func& func, In&& in) {
+        func(kj::mv(in));
+        return Void();
+    }
 };
 template <typename In>
 struct MaybeVoidCaller<In&, Void> {
-  template <typename Func>
-  static inline Void apply(Func& func, In& in) {
-    func(in);
-    return Void();
-  }
+    template <typename Func>
+    static inline Void apply(Func& func, In& in) {
+        func(in);
+        return Void();
+    }
 };
 template <>
 struct MaybeVoidCaller<Void, Void> {
-  template <typename Func>
-  static inline Void apply(Func& func, Void&& in) {
-    func();
-    return Void();
-  }
+    template <typename Func>
+    static inline Void apply(Func& func, Void&& in) {
+        func();
+        return Void();
+    }
 };
 
 template <typename T>
 inline T&& returnMaybeVoid(T&& t) {
-  return kj::fwd<T>(t);
+    return kj::fwd<T>(t);
 }
 inline void returnMaybeVoid(Void&& v) {}
 
@@ -220,24 +241,23 @@ using OwnPromiseNode = Own<PromiseNode, PromiseDisposer>;
 // PromiseNode uses a static disposer.
 
 class PromiseBase {
-public:
-  kj::String trace();
-  // Dump debug info about this promise.
+   public:
+    kj::String trace();
+    // Dump debug info about this promise.
 
-private:
-  OwnPromiseNode node;
+   private:
+    OwnPromiseNode node;
 
-  PromiseBase() = default;
-  PromiseBase(OwnPromiseNode&& node): node(kj::mv(node)) {}
+    PromiseBase() = default;
+    PromiseBase(OwnPromiseNode&& node) : node(kj::mv(node)) {}
 
-  template <typename>
-  friend class kj::Promise;
-  friend class PromiseNode;
+    template <typename>
+    friend class kj::Promise;
+    friend class PromiseNode;
 };
 
 void detach(kj::Promise<void>&& promise);
-void waitImpl(_::OwnPromiseNode&& node, _::ExceptionOrValue& result, WaitScope& waitScope,
-              SourceLocation location);
+void waitImpl(_::OwnPromiseNode&& node, _::ExceptionOrValue& result, WaitScope& waitScope, SourceLocation location);
 bool pollImpl(_::PromiseNode& node, WaitScope& waitScope, SourceLocation location);
 Promise<void> yield();
 Promise<void> yieldHarder();
@@ -245,19 +265,19 @@ OwnPromiseNode readyNow();
 OwnPromiseNode neverDone();
 
 class ReadyNow {
-public:
-  operator Promise<void>() const;
+   public:
+    operator Promise<void>() const;
 };
 
 class NeverDone {
-public:
-  template <typename T>
-  operator Promise<T>() const;
+   public:
+    template <typename T>
+    operator Promise<T>() const;
 
-  KJ_NORETURN(void wait(WaitScope& waitScope, SourceLocation location = {}) const);
+    KJ_NORETURN(void wait(WaitScope& waitScope, SourceLocation location = {}) const);
 };
 
-}  // namespace _ (private)
+}  // namespace _
 }  // namespace kj
 
 KJ_END_HEADER
